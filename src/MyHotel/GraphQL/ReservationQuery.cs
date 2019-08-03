9180eed9e68bbdee538e2ebf8de445a014e69c64
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-08-03 19:02:38 +0200

    allow multiple query params

--- src/MyHotel/GraphQL/ReservationQuery.cs
+++ src/MyHotel/GraphQL/ReservationQuery.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using GraphQL;
+﻿using GraphQL;
 using GraphQL.Types;
 using MyHotel.Entities;
 using MyHotel.GraphQL.Types;
 using MyHotel.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace MyHotel.GraphQL
 {
@@ -70,10 +70,10 @@ namespace MyHotel.GraphQL
                 }),
                 resolve: context =>
                 {
-                    var query = reservationRepository.GetQuery();
+                    var query = reservationRepository.GetQuery().AsQueryable();
 
                     var user = (ClaimsPrincipal)context.UserContext;
-                    var isUserAuthenticated = ((ClaimsIdentity) user.Identity).IsAuthenticated;
+                    var isUserAuthenticated = ((ClaimsIdentity)user.Identity).IsAuthenticated;
 
                     var reservationId = context.GetArgument<int?>("id");
                     if (reservationId.HasValue)
@@ -84,34 +84,31 @@ namespace MyHotel.GraphQL
                             return new List<Reservation>();
                         }
 
-                        return reservationRepository.GetQuery().Where(r => r.Id == reservationId.Value);
+                        query = query.Where(r => r.Id == reservationId.Value);
                     }
 
                     var checkinDate = context.GetArgument<DateTime?>("checkinDate");
                     if (checkinDate.HasValue)
                     {
-                        return reservationRepository.GetQuery()
-                            .Where(r => r.CheckinDate.Date == checkinDate.Value.Date);
+                        query = query.Where(r => r.CheckinDate.Date == checkinDate.Value.Date);
                     }
 
                     var checkoutDate = context.GetArgument<DateTime?>("checkoutDate");
                     if (checkoutDate.HasValue)
                     {
-                        return reservationRepository.GetQuery()
-                            .Where(r => r.CheckoutDate.Date >= checkoutDate.Value.Date);
+                        query = query.Where(r => r.CheckoutDate.Date >= checkoutDate.Value.Date);
                     }
 
                     var allowedSmoking = context.GetArgument<bool?>("roomAllowedSmoking");
                     if (allowedSmoking.HasValue)
                     {
-                        return reservationRepository.GetQuery()
-                            .Where(r => r.Room.AllowedSmoking == allowedSmoking.Value);
+                        query = query.Where(r => r.Room.AllowedSmoking == allowedSmoking.Value);
                     }
 
                     var roomStatus = context.GetArgument<RoomStatus?>("roomStatus");
                     if (roomStatus.HasValue)
                     {
-                        return reservationRepository.GetQuery().Where(r => r.Room.Status == roomStatus.Value);
+                        query = query.Where(r => r.Room.Status == roomStatus.Value);
                     }
 
                     return query.ToList();
