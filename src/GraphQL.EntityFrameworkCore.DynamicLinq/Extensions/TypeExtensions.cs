4ef685ce900a4e24e3da57b4b66c80244b573232
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-08-09 08:59:26 +0200

    .

--- src/GraphQL.EntityFrameworkCore.DynamicLinq/Extensions/TypeExtensions.cs
+++ src/GraphQL.EntityFrameworkCore.DynamicLinq/Extensions/TypeExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+#if !NET451
 using System.Reflection;
+#endif
 using GraphQL.Types;
 
 namespace GraphQL.EntityFrameworkCore.DynamicLinq.Extensions
@@ -14,12 +16,28 @@ namespace GraphQL.EntityFrameworkCore.DynamicLinq.Extensions
 
         public static bool IsObjectGraphType(this Type type)
         {
-            return type.GetTypeInfo().BaseType != null && type.GetTypeInfo().BaseType.Name == "ObjectGraphType`1";
+            Type baseType = type.GetTypeInfo().BaseType;
+            return baseType != null && baseType.Name == "ObjectGraphType`1";
         }
 
         public static Type ModelType(this Type type)
         {
             return type?.GraphType().GetTypeInfo().BaseType?.GetGenericArguments().First();
         }
+
+#if NET451
+        // https://github.com/castleproject/Core/blob/netcore/src/Castle.Core/Compatibility/IntrospectionExtensions.cs
+        // This allows us to use the new reflection API which separates Type and TypeInfo
+        // while still supporting .NET 3.5 and 4.0. This class matches the API of the same
+        // class in .NET 4.5+, and so is only needed on .NET Framework versions before that.
+        //
+        // Return the System.Type for now, we will probably need to create a TypeInfo class
+        // which inherits from Type like .NET 4.5+ and implement the additional methods and
+        // properties.
+        public static Type GetTypeInfo(this Type type)
+        {
+            return type;
+        }
+#endif
     }
 }
